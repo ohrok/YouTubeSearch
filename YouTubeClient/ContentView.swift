6f5b94b9fc720167d2634a77a71718bc9d0fb45a
ohrok <oh.hymh@gmail.com>
2021-05-08 15:07:41 +0900

    Initial Commit

---
+++ YouTubeClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YouTubeClient
+//
+//  Created by 大井裕貴 on 2021/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YouTubeClient/YouTubeClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeClientApp.swift
+//  YouTubeClient
+//
+//  Created by 大井裕貴 on 2021/05/08.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
