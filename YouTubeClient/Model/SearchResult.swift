fe45cfa2c163b3c868ac684b627abcaef6ae7938
ohrok <oh.hymh@gmail.com>
2021-05-11 18:03:20 +0900

    add model

---
+++ YouTubeClient/Model/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  YouTubeClient
+//
+//  Created by 大井裕貴 on 2021/05/11.
+//
+
+import Foundation
+
+struct SearchResult: Codable, Identifiable {
+    var id = UUID()
+    var name: String
+    var channelName: String
+}
--- YouTubeClient/Views/ContentView.swift
+++ YouTubeClient/Views/ContentView.swift
@@ -9,32 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     
-    private let videos: [String] = {
-        var videos: [String] = []
-        for i in 0..<1000 {
-            let video = "video\(i + 1)"
+    private let videos: [SearchResult] = {
+        var videos: [SearchResult] = []
+        let channelNameExamples: [String] = ["iOS Academy", "TOHO animation", "SUTEHAGE"]
+        for i in 0..<100 {
+            let video = SearchResult(name: "video\(i + 1)", channelName: channelNameExamples.randomElement() ?? "")
             videos.append(video)
         }
         return videos
     }()
     
-    @State private var searchedVideos: [String] = []
+    @State private var searchResults: [SearchResult] = []
     @State private var searchText: String = ""
     
     var body: some View {
         VStack {
             SearchBar(text: $searchText, placeholder: "Search Spots")
                 .onSearchBarSearchButtonClicked {
-                    searchedVideos = videos.filter {
-                        $0.lowercased().contains(self.searchText.lowercased())
+                    searchResults = videos.filter {
+                        $0.name.lowercased().contains(self.searchText.lowercased()) || $0.channelName.lowercased().contains(self.searchText.lowercased()) 
                     }
                     UIApplication.shared.closeKeyboard()
                 }
             List {
-                ForEach(searchedVideos, id: \.self) { video in
-                    Text(video)
+                ForEach(searchResults) { result in
+                    VStack(alignment: .leading) {
+                        Text(result.name).bold().foregroundColor(.red)
+                        Text(result.channelName)
+                    }
                 }
-                if searchedVideos.count == 0 {
+                if searchResults.count == 0 {
                     Text("Nothing Found")
                 }
             }
