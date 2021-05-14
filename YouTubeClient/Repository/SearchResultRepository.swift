ba1181e4d3dd2f4bcbcf2d7b3dc98518904dc07a
ohrok <oh.hymh@gmail.com>
2021-05-14 19:15:22 +0900

    add performRequest

--- YouTubeClient/Repository/SearchResultRepository.swift
+++ YouTubeClient/Repository/SearchResultRepository.swift
@@ -22,22 +22,34 @@ class SearchResultRepository: ObservableObject {
     }()
     
     func performSearch(for text: String) {
-        let _ = apiURL(searchText: text)
+        let url = apiURL(searchText: text)
+        print("URL: '\(url)'")
+        if let jsonString = performRequest(with: url) {
+            print("Received JSON string: '\(jsonString)'")
+        }
+        
         searchResults = videos.filter {
             $0.title.lowercased().contains(text.lowercased()) || $0.channelTitle.lowercased().contains(text.lowercased())
         }
     }
     
-    // Call API method
     private func apiURL(searchText: String) -> URL {
         let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         let maxResults = 50
-        let APIKey = "Axxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+        let APIKey = "Axxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         let urlString = "https://www.googleapis.com/youtube/v3/search?" +
                         "part=snippet&q=\(encodedText)&type=video" +
                         "&maxResults=\(maxResults)&key=\(APIKey)"
-        let url = URL(string: urlString)!
-        print("URL: \(url)")
-        return url
+        let url = URL(string: urlString)
+        return url!
+    }
+    
+    private func performRequest(with url: URL) -> String? {
+        do {
+            return try String(contentsOf: url, encoding: .utf8)
+        } catch {
+            print("Download Error: \(error.localizedDescription)")
+            return nil
+        }
     }
 }
